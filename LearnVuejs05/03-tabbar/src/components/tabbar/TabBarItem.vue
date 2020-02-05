68e68179503e85fbd8197f1af72be458a6538717
DwArFeng <915724865@qq.com>
2020-02-05 19:15:00 +0800

    学习笔记 20200205 更新
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ LearnVuejs05/03-tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @click="itemClick" class="tab-bar-item">
+    <div>
+      <div v-if="!isActive">
+        <slot name="item_icon"></slot>
+      </div>
+      <div v-else>
+        <slot name="item_icon_active"></slot>
+      </div>
+      <div :class="{active: isActive}">
+        <slot name="item_text"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String
+    },
+    data() {
+      return {
+        isActive: false
+      }
+    },
+    methods: {
+      itemClick: function () {
+        this.$router.replace(this.path, () => {
+        }, () => {
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+  }
+
+  .active {
+    color: #d81e06;
+  }
+</style>
